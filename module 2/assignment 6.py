e61a987650efe4f8f16068c07fbcb0679439961d
Sahana-Raghuraman <64693211+Sahana-Raghuraman@users.noreply.github.com>
2022-01-19 13:40:32 +0530

    Add files via upload

---
+++ module 2/assignment 6.py
@@ -0,0 +1,7 @@
+name = ("Sahana R")
+age = 20
+is_new = False
+items = [2,5,6,8,10]
+a = 98.3
+print("Medical Report", "\nName of the Patient :%s \nAge of the Patient :%s \nNew Patient : %s \nDates of visit : %s \nTemperatute of the Patient : %s" %\
+(name, age, is_new, items, a))
---
+++ module 2/assignment.py
@@ -0,0 +1,6 @@
+name = ("Sahana R")
+age = 20
+is_new = False
+items = [2,5,6,8,10]
+a = 98.3
+print("Medical Report", "\nName of the Patient :"  ,name, "\nAge of the Patient :"  ,age, "\nNew Patient : ",is_new, "\nDates of visit :" ,items, "\nTemperatute of the Patient :",a, "\nNo.of visits :" ,len(items))
